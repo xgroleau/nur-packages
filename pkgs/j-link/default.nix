302f540459ed0cf374170a023b81ceaec4d1239f
xgroleau 🐢 <xavgroleau@gmail.com>
2022-06-21 12:20:28 -0400

    Added support for arm64

--- pkgs/j-link/default.nix
+++ pkgs/j-link/default.nix
@@ -1,60 +1,84 @@
-{ lib, stdenv, writeText, fetchurl, udev }:
+{ lib, stdenv, writeText, fetchurl, autoPatchelfHook, fontconfig, freetype
+, libICE, libSM, libX11, libXcursor, libXext, libXfixes, libXrandr, libXrender
+, udev }:
 
 stdenv.mkDerivation rec {
   pname = "j-link";
   version = "7.66c";
 
+  urlOS = {
+    aarch64-linux = "Linux";
+    x86_64-linux = "Linux";
+  }.${stdenv.hostPlatform.system} or (throw
+    "Unsupported system: ${stdenv.hostPlatform.system}");
+
+  urlPlatform = {
+    aarch64-linux = "arm64";
+    x86_64-linux = "x86_64";
+  }.${stdenv.hostPlatform.system} or (throw
+    "Unsupported system: ${stdenv.hostPlatform.system}");
+
   src = fetchurl {
-    url = "https://www.segger.com/downloads/jlink/JLink_Linux_${
+    url = "https://www.segger.com/downloads/jlink/JLink_${urlOS}_V${
         (lib.replaceChars [ "." ] [ "" ] version)
-      }_x86_64.tgz";
-    sha256 = "y1IvmSWLARP91r3g1lp6YM/HuBDs3k6e4eTAsb1BNSg=";
-    curlOpts = [
-      "-d"
-      "@${
-        writeText "cdata.txt"
+      }_${urlPlatform}.tgz";
+
+    sha256 = {
+      aarch64-linux = "0fr8pki2g4bfk1rk90dzwql37d0b71ngzs9zyx0g2jainan3sqgv";
+      x86_64-linux = "q2yw/QuNUB8lWpd5dDx4J0fT5O+J6HZkoA0meT4/mQ8=";
+    }.${stdenv.hostPlatform.system} or (throw
+      "Unsupported system: ${stdenv.hostPlatform.system}");
+    curlOpts = "-d @${
+        writeText "curldata.txt"
         "accept_license_agreement=accepted&non_emb_ctr=confirmed&submit=Download+software"
-      }"
-    ];
+      }";
   };
 
-  rpath = lib.makeLibraryPath [ udev ] + ":${stdenv.cc.cc.lib}/lib64";
+  nativeBuildInputs = [
+    autoPatchelfHook
 
+  ];
+
+  buildInputs = [
+    fontconfig
+    freetype
+    libICE
+    libSM
+    libX11
+    libXcursor
+    libXext
+    libXfixes
+    libXrandr
+    libXrender
+    stdenv.cc.cc.lib
+    udev
+  ];
+
+  # Exe
   installPhase = ''
     mkdir -p $out/bin
-    mv Lib lib
     mv * $out
-    ln -s $out/JLink $out/bin
+    ln -s $out/JFlash* $out/bin/
+    ln -s $out/JLink* $out/bin/
+    ln -s $out/JMem* $out/bin/
+    ln -s $out/JRun* $out/bin/
+    ln -s $out/JTAGLoad* $out/bin/
+
   '';
 
-  postFixup = ''
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$out/JLink" \
-      --set-rpath ${rpath}:$out/lib "$out/JLink"
-    for file in $(find $out/lib -maxdepth 1 -type f -and -name \*.so\*); do
-      patchelf --set-rpath ${rpath}:$out/lib $file
-    done
+  # Udev rules
+  postInstall = ''
+    mkdir -p $out/udev
+    rules="$out/99-jlink.rules"
+    if [ ! -f "$rules" ]; then
+        echo "$rules is missing, must update the Nix file."
+        exit 1
+    fi
+    ln -s "$rules" "$out/etc/udev/rules.d/"
   '';
 
   meta = with lib; {
     description = "J-Link Debugger and Performance Analyzer";
-    longDescription = ''
-      JLink is a cross-platform debugger and performance analyzer for J-Link
-      and J-Trace.
-        - Stand-alone graphical debugger
-        - Debug output of any tool chain and IDE 1
-        - C/C++ source level debugging and assembly instruction debugging
-        - Debug information windows for any purpose: disassembly, memory,
-          globals and locals, (live) watches, CPU and peripheral registers
-        - Source editor to fix bugs immediately
-        - High-speed programming of the application into the target
-        - Direct use of J-Link built-in features (Unlimited Flash
-          Breakpoints, Flash Download, Real Time Terminal, Instruction Trace)
-        - Scriptable project files to set up everything automatically
-          - New project wizard to ease the basic configuration of new projects
-      1 JLink has been tested with the output of the following compilers:
-      GCC, Clang, ARM, IAR. Output of other compilers may be supported but is
-      not guaranteed to be.
-    '';
     homepage = "https://www.segger.com/products/debug-probes/j-link/";
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     license = licenses.unfree;
